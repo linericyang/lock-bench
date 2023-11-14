019f820e7b4045b821735bc940cced27361117aa
Lin Yang <lin.a.yang@intel.com>
2023-11-14 14:54:48 -0800

    Expand benchmark to measure RwLock performance
    Signed-off-by: Lin Yang <lin.a.yang@intel.com>

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,26 @@ fn main() {
         .map(|it| it.parse::<u32>().unwrap());
 
     let options = Options {
-        n_threads: args.next().unwrap(),
+        n_readers: args.next().unwrap(),
+        n_reads: args.next().unwrap(),
+        n_writers: args.next().unwrap(),
+        n_writes: args.next().unwrap(),
         n_locks: args.next().unwrap(),
-        n_ops: args.next().unwrap(),
         n_rounds: args.next().unwrap(),
     };
     println!("{:#?}\n", options);
 
-    bench::<mutexes::Std>(&options);
-    bench::<mutexes::ParkingLot>(&options);
-    bench::<mutexes::Spin>(&options);
-    bench::<mutexes::AmdSpin>(&options);
+    bench::<rwlocks::Std>(&options);
+    bench::<rwlocks::ParkingLot>(&options);
+    bench::<rwlocks::Spin>(&options);
 
     println!();
-    bench::<mutexes::Std>(&options);
-    bench::<mutexes::ParkingLot>(&options);
-    bench::<mutexes::Spin>(&options);
-    bench::<mutexes::AmdSpin>(&options);
+    bench::<rwlocks::Std>(&options);
+    bench::<rwlocks::ParkingLot>(&options);
+    bench::<rwlocks::Spin>(&options);
 }
 
-fn bench<M: Mutex>(options: &Options) {
+fn bench<M: RwLock>(options: &Options) {
     let mut times = (0..options.n_rounds)
         .map(|_| run_bench::<M>(options))
         .collect::<Vec<_>>();
@@ -52,9 +52,11 @@ fn bench<M: Mutex>(options: &Options) {
 
 #[derive(Debug)]
 struct Options {
-    n_threads: u32,
+    n_readers: u32,
+    n_reads: u32,
+    n_writers: u32,
+    n_writes: u32,
     n_locks: u32,
-    n_ops: u32,
     n_rounds: u32,
 }
 
@@ -68,33 +70,55 @@ fn random_numbers(seed: u32) -> impl Iterator<Item = u32> {
     })
 }
 
-trait Mutex: Sync + Send + Default {
+trait RwLock: Sync + Send + Default {
     const LABEL: &'static str;
-    fn with_lock(&self, f: impl FnOnce(&mut u32));
+    fn read(&self) -> u32;
+    fn write(&self, f: impl FnOnce(&mut u32));
 }
 
-fn run_bench<M: Mutex>(options: &Options) -> time::Duration {
+fn run_bench<M: RwLock>(options: &Options) -> time::Duration {
     let locks = &(0..options.n_locks)
         .map(|_| CachePadded::new(M::default()))
         .collect::<Vec<_>>();
 
-    let start_barrier = &Barrier::new(options.n_threads as usize + 1);
-    let end_barrier = &Barrier::new(options.n_threads as usize + 1);
+    let start_barrier = &Barrier::new(options.n_readers as usize + options.n_writers as usize+ 1);
+    let end_barrier = &Barrier::new(options.n_readers as usize + options.n_writers as usize+ 1);
 
     let elapsed = scope(|scope| {
-        let thread_seeds = random_numbers(0x6F4A955E).scan(0x9BA2BF27, |state, n| {
+        let reader_thread_seeds = random_numbers(0x6F4A955E).scan(0x9BA2BF27, |state, n| {
             *state ^= n;
             Some(*state)
         });
-        for thread_seed in thread_seeds.take(options.n_threads as usize) {
+        let writer_thread_seeds = random_numbers(0x6F4A955E).scan(0x9BA2BF27, |state, n| {
+            *state ^= n;
+            Some(*state)
+        });
+
+        for thread_seed in reader_thread_seeds.take(options.n_readers as usize) {
             scope.spawn(move |_| {
                 start_barrier.wait();
                 let indexes = random_numbers(thread_seed)
                     .map(|it| it % options.n_locks)
                     .map(|it| it as usize)
-                    .take(options.n_ops as usize);
+                    .take(options.n_reads as usize);
+                
                 for idx in indexes {
-                    locks[idx].with_lock(|cnt| *cnt += 1);
+                    locks[idx].read();
+                }
+                end_barrier.wait();
+            });
+        }
+
+        for thread_seed in writer_thread_seeds.take(options.n_writers as usize) {
+            scope.spawn(move |_| {
+                start_barrier.wait();
+                let indexes = random_numbers(thread_seed)
+                    .map(|it| it % options.n_locks)
+                    .map(|it| it as usize)
+                    .take(options.n_writes as usize);
+                
+                for idx in indexes {
+                    locks[idx].write(|cnt| *cnt += 1);
                 }
                 end_barrier.wait();
             });
@@ -108,9 +132,9 @@ fn run_bench<M: Mutex>(options: &Options) -> time::Duration {
 
         let mut total = 0;
         for lock in locks.iter() {
-            lock.with_lock(|cnt| total += *cnt);
+            total += lock.read();
         }
-        assert_eq!(total, options.n_threads * options.n_ops);
+        assert_eq!(total, options.n_writers * options.n_writes);
 
         elapsed
     })
@@ -118,106 +142,46 @@ fn run_bench<M: Mutex>(options: &Options) -> time::Duration {
     elapsed
 }
 
-mod mutexes {
-    use super::Mutex;
+mod rwlocks {
+    use super::RwLock;
 
-    pub(crate) type Std = std::sync::Mutex<u32>;
-    impl Mutex for Std {
-        const LABEL: &'static str = "std::sync::Mutex";
-        fn with_lock(&self, f: impl FnOnce(&mut u32)) {
-            let mut guard = self.lock().unwrap();
+    pub(crate) type Std = std::sync::RwLock<u32>;
+    impl RwLock for Std {
+        const LABEL: &'static str = "std::sync::RwLock";
+        fn read(&self) -> u32 {
+            let guard = self.read().unwrap();
+            *guard
+        }
+        fn write(&self, f: impl FnOnce(&mut u32)) {
+            let mut guard = self.write().unwrap();
             f(&mut guard)
         }
     }
 
-    pub(crate) type ParkingLot = parking_lot::Mutex<u32>;
-    impl Mutex for ParkingLot {
-        const LABEL: &'static str = "parking_lot::Mutex";
-        fn with_lock(&self, f: impl FnOnce(&mut u32)) {
-            let mut guard = self.lock();
+    pub(crate) type ParkingLot = parking_lot::RwLock<u32>;
+    impl RwLock for ParkingLot {
+        const LABEL: &'static str = "parking_lot::RwLock";
+        fn read(&self) -> u32 {
+            let guard = self.read();
+            *guard
+        }
+        fn write(&self, f: impl FnOnce(&mut u32)) {
+            let mut guard = self.write();
             f(&mut guard)
         }
     }
 
-    pub(crate) type Spin = spin::Mutex<u32>;
-    impl Mutex for Spin {
-        const LABEL: &'static str = "spin::Mutex";
-        fn with_lock(&self, f: impl FnOnce(&mut u32)) {
-            let mut guard = self.lock();
+    pub(crate) type Spin = spin::RwLock<u32>;
+    impl RwLock for Spin {
+        const LABEL: &'static str = "spin::RwLock";
+        fn read(&self) -> u32 {
+            let guard = self.read();
+            *guard
+        }
+        fn write(&self, f: impl FnOnce(&mut u32)) {
+            let mut guard = self.write();
             f(&mut guard)
         }
     }
 
-    pub(crate) type AmdSpin = crate::amd_spinlock::AmdSpinlock<u32>;
-    impl Mutex for AmdSpin {
-        const LABEL: &'static str = "AmdSpinlock";
-        fn with_lock(&self, f: impl FnOnce(&mut u32)) {
-            let mut guard = self.lock();
-            f(&mut guard)
-        }
-    }
-}
-
-mod amd_spinlock {
-    use std::{
-        cell::UnsafeCell,
-        ops,
-        sync::atomic::{spin_loop_hint, AtomicBool, Ordering},
-    };
-
-    #[derive(Default)]
-    pub(crate) struct AmdSpinlock<T> {
-        locked: AtomicBool,
-        data: UnsafeCell<T>,
-    }
-    unsafe impl<T: Send> Send for AmdSpinlock<T> {}
-    unsafe impl<T: Send> Sync for AmdSpinlock<T> {}
-
-    pub(crate) struct AmdSpinlockGuard<'a, T> {
-        lock: &'a AmdSpinlock<T>,
-    }
-
-    impl<T> AmdSpinlock<T> {
-        pub(crate) fn lock(&self) -> AmdSpinlockGuard<T> {
-            loop {
-                let was_locked = self.locked.load(Ordering::Relaxed);
-                if !was_locked
-                    && self
-                        .locked
-                        .compare_exchange_weak(
-                            was_locked,
-                            true,
-                            Ordering::Acquire,
-                            Ordering::Relaxed,
-                        )
-                        .is_ok()
-                {
-                    break;
-                }
-                spin_loop_hint()
-            }
-            AmdSpinlockGuard { lock: self }
-        }
-    }
-
-    impl<'a, T> ops::Deref for AmdSpinlockGuard<'a, T> {
-        type Target = T;
-        fn deref(&self) -> &Self::Target {
-            let ptr = self.lock.data.get();
-            unsafe { &*ptr }
-        }
-    }
-
-    impl<'a, T> ops::DerefMut for AmdSpinlockGuard<'a, T> {
-        fn deref_mut(&mut self) -> &mut Self::Target {
-            let ptr = self.lock.data.get();
-            unsafe { &mut *ptr }
-        }
-    }
-
-    impl<'a, T> Drop for AmdSpinlockGuard<'a, T> {
-        fn drop(&mut self) {
-            self.lock.locked.store(false, Ordering::Release)
-        }
-    }
 }
